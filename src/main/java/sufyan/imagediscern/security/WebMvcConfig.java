8412e7292e74bb6029b5c8b6665106034e9147dc
Sufyan <khotsufyan@gmail.com>
2022-08-02 20:33:48 +0530

    For CORS allowed backend URL

--- src/main/java/sufyan/imagediscern/security/WebMvcConfig.java
+++ src/main/java/sufyan/imagediscern/security/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/image/**")
-	           	.allowedOrigins("http://localhost:3000")
+	           	.allowedOrigins("http://localhost:3000", "https://image2text-frontend.herokuapp.com")
 	           	.allowedMethods("GET", "POST")
 	           	.allowedHeaders("*")
 	           	.allowCredentials(false).maxAge(3600);
